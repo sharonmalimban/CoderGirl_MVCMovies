6679f9e8e8e2cd7bc06edc5f2c40c94bd64346c6
dwmoeller1 <dwmoeller1@aol.com>
2019-05-01 17:47:04 -0500

    REmove todos

--- CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
+++ CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
@@ -24,10 +24,7 @@ namespace CoderGirl_MVCMovies.Controllers
                 <button type='submit'>Rate it</button>
             </form>";
 
-        /// TODO: Create a view Index. This view should list a table of all saved movie names with associated average rating
-        /// TODO: Be sure to include headers for Movie and Rating
-        /// TODO: Each tr with a movie rating should have an id attribute equal to the id of the movie rating
-        public IActionResult Index()
+       public IActionResult Index()
         {
             List<int> ids = repository.GetIds();
             var movieRatings = ids.Select(id => repository.GetMovieNameById(id))
@@ -38,9 +35,6 @@ namespace CoderGirl_MVCMovies.Controllers
             return View();
         }
 
-        // TODO: Create a view MovieRating/Create and put the htmlForm there. Remember that html in a view should not be a string.
-        // TODO: Change the input tag for movie name to be a drop down which has a list of movies from the movie repository
-        // TODO: Change this method to return that view. 
         [HttpGet]
         public IActionResult Create()
         {
@@ -48,7 +42,6 @@ namespace CoderGirl_MVCMovies.Controllers
             return View();
         }
 
-        // TODO: Save the movie/rating in the MovieRatingRepository before redirecting to the Details page
         [HttpPost]
         public IActionResult Create(string movieName, string rating)
         {
@@ -56,9 +49,6 @@ namespace CoderGirl_MVCMovies.Controllers
             return RedirectToAction(actionName: nameof(Details), routeValues: new { movieName, rating });
         }
 
-        // TODO: The Details method should take an int parameter which is the id of the movie/rating to display.
-        // TODO: Create a Details view which displays the formatted string with movie name and rating in an h2 tag. 
-        // TODO: The Details view should include a link to the MovieRating/Index page
         [HttpGet]
         public IActionResult Details(string movieName, string rating)
         {
