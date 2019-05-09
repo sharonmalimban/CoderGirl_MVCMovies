b166d9947db0c8d895f1c8f10c9c1407b839291b
dwmoeller1 <dwmoeller1@aol.com>
2019-05-08 20:18:50 -0500

    In class work

--- CoderGirl_MVCMovies/Models/MovieRating.cs
+++ CoderGirl_MVCMovies/Models/MovieRating.cs
@@ -10,5 +10,6 @@ namespace CoderGirl_MVCMovies.Models
         public int Id { get; set; }
         public string MovieName { get; set; }
         public int Rating { get; set; }
+        public int MovieId { get; set; }
     }
 }
