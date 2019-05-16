a0459a4e97f40fcdfdec64e9126ecb07bd7b8c0c
rosesharondi@icloud.com <rosesharondi@icloud.com>
2019-05-15 19:26:22 -0500

    Classwork 0515

--- CoderGirl_MVCMovies/Controllers/MovieController.cs
+++ CoderGirl_MVCMovies/Controllers/MovieController.cs
@@ -29,6 +29,21 @@ namespace CoderGirl_MVCMovies.Controllers
         [HttpPost]
         public IActionResult Create(Movie movie)
         {
+			if (string.IsNullOrWhiteSpace(movie.Name))
+			{
+				ModelState.AddModelError("Name", "Name must be included");
+			}
+
+			if (movie.Year < 1888 || movie.Year > DateTime.Now.Year)
+			{
+				ModelState.AddModelError("Year", "Year is not valid");				
+			}
+
+			if (ModelState.ErrorCount > 0)
+			{
+				ViewBag.Directors = directorRepository.GetDirectors();
+				return View(movie);
+			}
             movieRepository.Save(movie);
             return RedirectToAction(actionName: nameof(Index));
         }
